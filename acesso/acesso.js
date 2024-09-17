d329eeab17e7c30381b58db781595d057a5d5a89
Danilo <77410497+DaniloRds@users.noreply.github.com>
2024-09-16 21:13:12 -0300

    up

---
+++ acesso/acesso.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector("#btn");
+
+function send() {
+    const usuario = document.querySelector(".usuario").value;
+    const senha = document.querySelector(".senha").value;
+    const preencha = document.querySelector("#preencha");
+    const sucesso = document.querySelector("#sucesso");
+    const negado = document.querySelector("#negado"); 
+
+    if (usuario.length < 1 || senha.length < 1) {
+        preencha.style.display = 'inline';
+
+        setTimeout(() => {
+            preencha.style.display = 'none';
+        }, 1000);
+
+        return false;
+    }
+
+    if (usuario === "sarah" && senha === "170224") {
+        sucesso.style.display = 'inline';
+
+        setTimeout(() => {
+            location.href = "home/home.html";
+        }, 500);
+    } else {
+        negado.style.display = 'inline';
+
+        setTimeout(() => {
+            location.href = "error/erro.html";
+        }, 500);
+    }
+}
+
+btn.addEventListener("click", send);
